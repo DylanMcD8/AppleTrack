9d2462e594c9307d142de95393881e2291a9ac4d
Dylan McDonald <dylan@dlmconsulting.org>
2022-07-31 23:13:24 -0400

    – Fixed Mac scaling issues – Added sample data function

--- AppleTrack/Settings/AboutTableViewController.swift
+++ AppleTrack/Settings/AboutTableViewController.swift
@@ -61,6 +61,36 @@ class AboutTableViewController: UITableViewController, SFSafariViewControllerDel
             }
         }
         tableView.deselectRow(at: indexPath, animated: true)
+		
+		if indexPath.section == 6 {
+			let alertController = UIAlertController(title: "Are you sure?", message: "This will add sample data to the app. This is not intended for general users. You should delete existing entries before doing this.", preferredStyle: .alert)
+			
+			let action2 = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction) in
+			}
+			
+			let action1 = UIAlertAction(title: "Apply Sample Data", style: .destructive) { (action:UIAlertAction) in
+				let dateFormatter = DateFormatter()
+				dateFormatter.dateStyle = .long
+				let sampleImage = UIImage(named: "Fallback Image")!.pngData()!
+//				createNewSavedDevice(productLine: "Apple TV", modelName: "Apple TV 4K", announcementDate: dateFormatter.date(from: "September 12, 2017")!, releaseDate: Date(), purchaseDate: Date(), color: "Black", capacity: "32 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "MacBook Pro", modelName: "MacBook Pro (16-inch, 2021)", announcementDate: dateFormatter.date(from: "October 18, 2021")!, releaseDate: Date(), purchaseDate: Date(), color: "Space Gray", capacity: "1 TB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "MacBook Pro", modelName: "MacBook Pro (15-inch, 2017)", announcementDate: dateFormatter.date(from: "June 5, 2017")!, releaseDate: Date(), purchaseDate: Date(), color: "Space Gray", capacity: "512 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "iPhone", modelName: "iPhone 13 Pro Max", announcementDate: dateFormatter.date(from: "September 14, 2021")!, releaseDate: Date(), purchaseDate: Date(), color: "Sierra Blue", capacity: "512 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "iPhone", modelName: "iPhone 6", announcementDate: dateFormatter.date(from: "September 9, 2014")!, releaseDate: Date(), purchaseDate: Date(), color: "Silver", capacity: "16 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "iPhone", modelName: "iPhone", announcementDate: dateFormatter.date(from: "January 7, 2007")!, releaseDate: Date(), purchaseDate: Date(), color: "Silver", capacity: "4 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "iPod classic", modelName: "iPod (5th Generation)", announcementDate: dateFormatter.date(from: "October 12, 2005")!, releaseDate: Date(), purchaseDate: Date(), color: "White", capacity: "80 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "iPod classic", modelName: "iPod (3rd Generation)", announcementDate: dateFormatter.date(from: "July 14, 2004")!, releaseDate: Date(), purchaseDate: Date(), color: "White", capacity: "40 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "iPad mini", modelName: "iPad mini (6th Generation)", announcementDate: dateFormatter.date(from: "September 14, 2021")!, releaseDate: Date(), purchaseDate: Date(), color: "Purple", capacity: "256 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				createNewSavedDevice(productLine: "iPad mini", modelName: "iPad mini", announcementDate: dateFormatter.date(from: "October 23, 2012")!, releaseDate: Date(), purchaseDate: Date(), color: "Purple", capacity: "256 GB", serialNumber: "XXXXXXXXXX", notes: "", imageData: sampleImage)
+				
+			}
+			
+			alertController.addAction(action1)
+			alertController.addAction(action2)
+			alertController.actions[0].setValue(UIColor(named: "AccentColor")!, forKey: "titleTextColor")
+			alertController.view.tintColor = UIColor(named: "AccentColor")!
+			self.present(alertController, animated: true, completion: nil)
+		}
     }
     
     override func viewDidAppear(_ animated: Bool) {
